3c155dd4632ce9cf3adf08c78961d2c11294a0d2
qlemql <taehyun_fe@naver.com>
2022-08-27 23:16:29 +0900

    ♻️ lint warning 해결

--- src/components/ui/atoms/Button/Button.tsx
+++ src/components/ui/atoms/Button/Button.tsx
@@ -3,22 +3,20 @@ import React from 'react';
 import * as S from './styled';
 
 interface IProps {
-  children?: React.ReactNode;
+  children: React.ReactNode;
   onClick: () => void;
 }
 
-const Button: React.FC<IProps> = ({ children, onClick }) => {
-  return (
-    <S.Div
-      backgroundColor={'#000000'}
-      color={`#BECCE9`}
-      hoverBackgroundColor={`#7D70FA`}
-      hoverColor={`#D9FD89`}
-      onClick={onClick}
-    >
-      {children}
-    </S.Div>
-  )
-};
+const Button: React.FC<IProps> = ({ children, onClick }) => (
+  <S.Div
+    backgroundColor="#000000"
+    color="#BECCE9"
+    hoverBackgroundColor="#7D70FA"
+    hoverColor="#D9FD89"
+    onClick={onClick}
+  >
+    {children}
+  </S.Div>
+);
 
 export default Button;
