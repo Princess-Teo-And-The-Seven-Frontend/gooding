32137f932ab4ed950bd1a0002881cd9b245b0362
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2022-08-27 23:54:34 +0900

    📝 index에 MyInfo 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import type { NextPage } from 'next';
 
 import Header from '@/components/features/Header';
+import MyInfo from '@/components/features/MyInfo';
 
 const HomePage: NextPage = () => (
   <div>
     <Header />
+    <div>
+      <MyInfo />
+    </div>
   </div>
 );
 
