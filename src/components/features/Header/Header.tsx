39e07d52ffcbd1a4cb6457b93632cb6181feb624
bananajeong <73640737+Najeong-Kim@users.noreply.github.com>
2022-08-27 18:12:27 +0900

    ✨ Header 구현
    Co-authored-by: zzinao <zzinao@users.noreply.github.com>
    Co-authored-by: ryuna <anottrx@gmail.com>

---
+++ src/components/features/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Button from '@components/ui/atoms/Button'
+
+import * as S from './styled';
+
+const Header = () => {
+  const [isMain, setIsMain] = React.useState(true) //밖으로 뺄지 나중에 고민
+  const handleToggleMenu = () => {
+    setIsMain(!isMain)
+  }
+  
+  return (
+    <S.HeaderContainer>
+      <S.Logo src="gooding_logo.png" alt="구딩 로고" />
+      <Button onClick={handleToggleMenu}>{isMain ? '모아보기' : '캘린더'}</Button>
+    </S.HeaderContainer>
+  )
+};
+
+export default Header;
