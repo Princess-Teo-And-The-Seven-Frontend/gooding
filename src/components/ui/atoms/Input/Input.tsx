4fbc4ae5b53bbcd2923b68ffe2f10a6e9a1655d1
qlemql <taehyun_fe@naver.com>
2022-08-27 23:26:56 +0900

    ✨ create new Input atom
    Co-authored-by: Junseongparkme <junseongpark.dev@gmail.com>

--- src/components/ui/atoms/Input/Input.tsx
+++ src/components/ui/atoms/Input/Input.tsx
@@ -1,3 +1,28 @@
 import React from 'react';
+import * as S from './styled';
 
-export const Input = () => <div>Input</div>;
+interface InputProps {
+    inputName: string;
+    isError:boolean;
+    borderColor: 'black';
+    color: 'black' | 'red' | 'grey';
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
+    onKeyDown: React.KeyboardEventHandler<HTMLInputElement>
+    onFocus: () => void;
+    placeholder: string;
+}
+
+export const Input: React.FC<InputProps> = ({
+  inputName, isError, borderColor, color, onChange, onFocus, onKeyDown, placeholder,
+}) => (
+  <S.Input
+    isError={isError}
+    borderColor={borderColor}
+    color={color}
+    name={inputName}
+    onChange={onChange}
+    onKeyDown={onKeyDown}
+    onFocus={onFocus}
+    placeholder={placeholder}
+  />
+);
