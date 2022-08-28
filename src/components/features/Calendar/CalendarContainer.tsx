ef3bbd7bee581973a4883921385f7a3994277fbc
zzinao <kjn0265@gmail.com>
2022-08-28 22:21:45 +0900

    📝  메인화면 마이페이지 주석처리

--- src/components/features/Calendar/CalendarContainer.tsx
+++ src/components/features/Calendar/CalendarContainer.tsx
@@ -1,13 +1,15 @@
-import MyInfo from '@/components/features/MyInfo';
+// import MyInfo from '@/components/features/MyInfo';
 import { BigCalendar } from './BigCalendar';
 
 import * as S from './styled';
 
+// 빌드 에러때문에 주석처리 했습니다
+
 export const CalendarContainer = () => (
   <S.Container>
-    <S.MyInfoContainer>
+    {/* <S.MyInfoContainer>
       <MyInfo />
-    </S.MyInfoContainer>
+    </S.MyInfoContainer> */}
     <BigCalendar />
   </S.Container>
 );
