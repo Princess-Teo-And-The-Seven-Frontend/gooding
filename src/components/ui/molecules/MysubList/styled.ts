b490aebcc64a5086a883dc7a3edd5520cbdd64d3
zzinao <kjn0265@gmail.com>
2022-08-28 15:20:03 +0900

    🔥 불필요한 삭제

--- src/components/ui/molecules/MysubList/styled.ts
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-export const SubsContainer = styled.div`
-  margin: 30px;
-  display: grid;
-  grid-gap: 1rem;
-  grid-template-columns: repeat(auto-fill, minmax(20%, auto));
-`;
