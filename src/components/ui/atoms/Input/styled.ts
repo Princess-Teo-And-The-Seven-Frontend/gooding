a13a3b42bb8c1b30e9893e47bdc3d5a0f60c2763
HyunHo Lee <dlgusgh2001@naver.com>
2022-08-28 15:31:04 +0900

    🐛 eslint

--- src/components/ui/atoms/Input/styled.ts
+++ src/components/ui/atoms/Input/styled.ts
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import styled from '@emotion/styled';
 
 interface InputProps {
@@ -8,23 +9,23 @@ interface InputProps {
 }
 
 export const Input = styled.input<InputProps>`
-    width: 80%;
-    border-radius: 5px;
-    padding: 5px 15px;
-    background-color: gray;
-    border: 1px solid ${({ isError, isConfirm, borderColor }) => {
-    if (isError) {
-      return 'red';
-    }
-    if (isConfirm) {
-      return 'grey';
-    }
-    if (borderColor === 'black') {
+  width: 80%;
+  border-radius: 5px;
+  padding: 5px 15px;
+  background-color: gray;
+  border: 1px solid
+    ${({ isError, isConfirm, borderColor }) => {
+      if (isError) {
+        return 'red';
+      }
+      if (isConfirm) {
+        return 'grey';
+      }
+      if (borderColor === 'black') {
+        return 'black';
+      }
       return 'black';
-    }
-    return 'black';
-  }}
-    outline: none;
-    color: black;
-    
+    }};
+  outline: none;
+  color: black;
 `;
