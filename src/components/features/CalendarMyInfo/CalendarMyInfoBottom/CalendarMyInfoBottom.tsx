2b34ea432236398be3026ef305c3559d4b436485
easyone <thinkanddoit@naver.com>
2022-08-29 03:42:06 +0900

    ✨ 캘린더 페이지 좌측 뷰 구현
    Co-authored-by: ryuna <anottrx@gmail.com>
    - 구독 정보 변경시 컴포넌트 렌더링
    - 남은 구독료 계산 비즈니스 로직 추가

--- src/components/features/CalendarMyInfo/CalendarMyInfoBottom/CalendarMyInfoBottom.tsx
+++ src/components/features/CalendarMyInfo/CalendarMyInfoBottom/CalendarMyInfoBottom.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-// import CalendarMyInfoBottomDetail from './CalendarMyInfoBottomDetail';
+import { ServiceDataType } from '@/types/dataType';
+import CalendarMyInfoBottomDetail from './CalendarMyInfoBottomDetail';
 
 import * as S from './styled';
 
-const CalendarMyInfoBottom: React.FC = () => (
-  <S.DetailContainer>
-    {/* <S.Amount>다음 결제까지...</S.Amount>
-    {scheduledToPayData.map(service => (
-      <CalendarMyInfoBottomDetail scheduledToPayService={service} />
-    ))} */}
-  </S.DetailContainer>
-);
+const CalendarMyInfoBottom = (data: ServiceDataType[]) => {
+  const today = new Date();
+  const getLeftDay = (date: string) => Number(date.slice(-2)) - today.getDate();
+
+  const sortedData = Object.values(data)
+    // eslint-disable-next-line arrow-parens
+    .filter(service => getLeftDay(service.date) >= 0)
+    .sort((a, b) => getLeftDay(a.date) - getLeftDay(b.date));
+
+  return (
+    <S.BottomContainer>
+      {sortedData.length ? (
+        <S.Amount>다음 결제까지...</S.Amount>
+      ) : (
+        <S.Amount>이번달 남은 구독료는 없어요!</S.Amount>
+      )}
+      {sortedData.map(({ ...service }) => (
+        <CalendarMyInfoBottomDetail
+          key={`CalendarMyInfoBottom-${service.id}`}
+          image={service.image}
+          name={service.name}
+          leftDay={getLeftDay(service.date)}
+        />
+      ))}
+    </S.BottomContainer>
+  );
+};
 
 export default CalendarMyInfoBottom;
