901939d1de0941b1250e2f0ec59530f6b4dcd8b6
ryuna <anottrx@gmail.com>
2022-08-28 11:26:26 +0900

    📝 isUserSubsCount 추가

--- src/components/features/MyInfo/MyInfoTop/MyInfoTop.tsx
+++ src/components/features/MyInfo/MyInfoTop/MyInfoTop.tsx
@@ -2,24 +2,30 @@ import React from 'react';
 
 import * as S from './styled';
 
+interface IMyInfo {
+  topPercent: number;
+  nickname: string;
+}
+
+const initialMyInfo = {
+  topPercent: 100,
+  nickname: '예비구독러',
+};
+
 const MyInfoTop = () => {
-  const [topPercent, setTopPercent] = React.useState(100);
-  const [nickname, setNickname] = React.useState('예비 구독러');
+  const [myInfo, setMyInfo] = React.useState<IMyInfo>(initialMyInfo);
 
   React.useEffect(() => {
     const userSubsCount = window.localStorage.getItem('userInfo')?.match(/}/g)?.length;
-    if (userSubsCount !== undefined && userSubsCount > 0) {
-      setTopPercent(60);
-      setNickname('찍먹 구독러');
-    } else if (userSubsCount !== undefined && userSubsCount <= 2) {
-      setTopPercent(50);
-      setNickname('평균 구독러');
-    } else if (userSubsCount !== undefined && userSubsCount <= 6) {
-      setTopPercent(20);
-      setNickname('프로 구독러');
-    } else if (userSubsCount !== undefined && userSubsCount > 6) {
-      setTopPercent(2);
-      setNickname('부자 구독러');
+    const isUserSubsCount = userSubsCount !== undefined;
+    if (isUserSubsCount && userSubsCount > 0) {
+      setMyInfo({ topPercent: 60, nickname: '찍먹 구독러' });
+    } else if (isUserSubsCount && userSubsCount <= 2) {
+      setMyInfo({ topPercent: 50, nickname: '평균 구독러' });
+    } else if (isUserSubsCount && userSubsCount <= 6) {
+      setMyInfo({ topPercent: 20, nickname: '프로 구독러' });
+    } else if (isUserSubsCount && userSubsCount > 6) {
+      setMyInfo({ topPercent: 2, nickname: '부자 구독러' });
     }
   }, []);
 
@@ -28,10 +34,10 @@ const MyInfoTop = () => {
       <S.MyInfoTop>
         <S.YouText>당신은...</S.YouText>
         <S.MyInfoText marginTop="12" fontSize="20">
-          {`상위 ${topPercent}%`}
+          {`상위 ${myInfo.topPercent}%`}
         </S.MyInfoText>
         <S.MyInfoText marginTop="8" fontSize="36">
-          {`${nickname}!`}
+          {`${myInfo.nickname}!`}
         </S.MyInfoText>
       </S.MyInfoTop>
     </S.CardContainer>
