591b6100efa11601da01fdecac5125e52c049d5b
HyunHo Lee <dlgusgh2001@naver.com>
2022-08-28 17:20:49 +0900

    🚑️ sublist typescript

---
+++ src/icons/PlusIcon.tsx
@@ -0,0 +1,15 @@
+/* eslint-disable max-len */
+import * as React from 'react';
+import { SVGProps } from 'react';
+
+export const PlusIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 52 52"
+    style={{}}
+    xmlSpace="preserve"
+    {...props}
+  >
+    <path d="M26 0C11.664 0 0 11.663 0 26s11.664 26 26 26 26-11.663 26-26S40.336 0 26 0zm12.5 28H28v11a2 2 0 0 1-4 0V28H13.5a2 2 0 0 1 0-4H24V14a2 2 0 0 1 4 0v10h10.5a2 2 0 0 1 0 4z" />
+  </svg>
+);
