371f3e6025d9c717b41a0c5ee2e178cbe6aece37
ryuna <anottrx@gmail.com>
2022-08-29 02:44:36 +0900

    ➕ 메타 태그 설정
    Co-authored-by: JIWON LEE <thinkanddoit@users.noreply.github.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { Global } from '@emotion/react';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 import { reset } from '@/styles/Reset';
 import { RecoilRoot } from 'recoil';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <RecoilRoot>
+    <Head>
+      <meta charSet="utf-8" />
+      <link rel="icon" href="gooding_favicon_logo.png" />
+      <title>구딩</title>
+      <meta name="description" content="내 구독 관리의 시작, 이제는 구딩에서!" />
+    </Head>
     <Global styles={reset} />
     <Component {...pageProps} />
   </RecoilRoot>
